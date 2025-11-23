240be45f93f4037250db74f13e4defb91086fd62
MingukJeon <rzbsys@unist.ac.kr>
2025-11-23 09:16:03 +0000

    lint

--- src/utils.py
+++ src/utils.py
@@ -9,21 +9,29 @@ def set_seed(seed: int = 42):
     random.seed(seed)
     np.random.seed(seed)
 
+
 def freeze_parameters(model):
     for name, param in model.named_parameters():
         param.requires_grad = False
     return model
+
 
 def unfreeze_parameters(model):
     for name, param in model.named_parameters():
         param.requires_grad = True
     return model
 
+
 def to_device(batch, device):
     new_batch = {}
     for k, v in batch.items():
+        # print("???", k)
         if isinstance(v, torch.Tensor):
             new_batch[k] = v.to(device)
+        elif isinstance(v, dict):
+            # print(k)
+            new_batch[k] = to_device(v, device)
         else:
+            # print("?", k)
             new_batch[k] = v
     return new_batch
